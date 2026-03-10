54ecfd67b4fb006a37f5251d77e6818e3f670c27
Lei Nelissen <lei@codified.nl>
2026-03-10 09:02:19 +0100

    fix: full text search

--- src/store/database/migrations/migrations.js
+++ src/store/database/migrations/migrations.js
@@ -1,10 +1,9 @@
-// This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
+import m0000 from "./20260308083649_strong_johnny_storm/migration.sql";
+import m0001 from "./20260310075859_fts_search/migration.sql";
 
-import m0000 from './20260308083649_strong_johnny_storm/migration.sql';
-
-  export default {
+export default {
     migrations: {
-      "20260308083649_strong_johnny_storm": m0000
-}
-  }
-  
+        "20260308083649_strong_johnny_storm": m0000,
+        "20260310075859_fts_search": m0001,
+    },
+};
